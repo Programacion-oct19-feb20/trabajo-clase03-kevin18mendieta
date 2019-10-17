dec821b14fc3b989693f7fb19f48ad94327830a1
kevinmendeita <mendietakevin144@gmail.com>
2019-10-17 12:48:51 -0500

    ejemplo2.java

---
+++ Ejemplo2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Ejemplo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombres;
+        nombres = "Kevin Guillermo";
+        System.out.println(nombres);
+        
+        String apellidos;
+        apellidos = "Mendieta Gahona";
+        System.out.println(apellidos);
+        
+        int edad;
+        edad = 19;
+        System.out.println(edad);
+        
+        String universidad;
+        universidad = "universidad tecnica particular de loja";
+        System.out.println(universidad);
+        
+    }
+    
+}
